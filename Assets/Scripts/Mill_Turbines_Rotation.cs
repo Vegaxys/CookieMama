47f15b6390fb220d24f17a99bc9874c156d63e1a
JimZucci <jimzucci5@gmail.com>
2019-05-31 09:33:26 +0100

    scene jimmy + intégration 3D

---
+++ Assets/Scripts/Mill_Turbines_Rotation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mill_Turbines_Rotation : MonoBehaviour
+{
+    void Update()
+    {
+        gameObject.transform.Rotate(Vector3.right * Time.deltaTime * 150);
+    }
+}
