490b09a696ddddd49f88b501cff19adc99661285
JimZucci <jimzucci5@gmail.com>
2019-05-31 11:11:42 +0100

    3D intégration 2

--- Assets/Scripts/Mill_Turbines_Rotation.cs
+++ Assets/Scripts/Mill_Turbines_Rotation.cs
@@ -6,6 +6,6 @@ public class Mill_Turbines_Rotation : MonoBehaviour
 {
     void Update()
     {
-        gameObject.transform.Rotate(Vector3.right * Time.deltaTime * 150);
+        gameObject.transform.Rotate(Vector3.forward * Time.deltaTime * 100);
     }
 }
