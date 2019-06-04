28cd47bf31477663f5a14fc4f716e9821bcf93b2
Julien <julien.kopoin@gmail.com>
2019-06-04 10:31:11 +0200

    4000 fichiers yes !

---
+++ Assets/Scripts/UI_Interaction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[System.Serializable]
+class PlaceToBe
+{
+    public string name;
+    public GameObject place;
+    public Transform spawnPoint;
+    public bool isOutDoor;
+}
+
+public class UI_Interaction : MonoBehaviour{
+    [SerializeField] GameObject player;
+    [SerializeField] List<PlaceToBe> locations;
+    int places;
+
+    public void TeleportTo(int index) {
+        locations[places].place.SetActive(false);
+        locations[index].place.SetActive(true);
+        player.transform.position = locations[index].spawnPoint.position;
+        places = index;
+    }
+}
