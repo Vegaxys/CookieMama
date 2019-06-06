ea6ccc1e7dd07bb79113bcd94b9236ff36d48094
Meaux Saint Marc Philippine <philippinemsm@laposte.net>
2019-06-06 10:37:58 +0200

    Affichages des ressources sur les panneaux

---
+++ Assets/Scripts/PrintVariable.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class PrintVariable : MonoBehaviour
+{
+    public Ressources_Manager ressourceManager;
+    public string textToAdd;
+    TextMeshProUGUI text;
+    public string var;
+    int value;
+
+    private void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        value = (int)ressourceManager.GetType().GetField(var).GetValue(ressourceManager);
+        SetDisplay(value, text, textToAdd);
+        Debug.Log(value);
+    }
+
+    void SetDisplay(int varToPrint, TextMeshProUGUI text, string textToAdd)
+    {
+        text.text = textToAdd + " " + varToPrint.ToString();
+    }
+}
