efd9815e8fd0f360fa9365ea7a3a95222d390c89
Julien <julien.kopoin@gmail.com>
2019-06-06 11:40:53 +0200

    changement sur la scene principale

--- Assets/Scripts/UI_Interaction.cs
+++ Assets/Scripts/UI_Interaction.cs
@@ -14,11 +14,17 @@ public class UI_Interaction : MonoBehaviour{
 
     [SerializeField] GameObject player;
     [SerializeField] List<PlaceToBe> locations;
-    int places;
 
+    public GameObject cuisineExt, cuisineInt;
+    int places;
+    // 0 = interieur
+    // 1 = exterieur
     public void TeleportTo(int index) {
         player.transform.position = locations[index].spawnPoint.position;
         places = index;
     }
-
+    public void ToogleCuisine() {
+        cuisineExt.SetActive(!cuisineExt.activeInHierarchy);
+        cuisineInt.SetActive(!cuisineInt.activeInHierarchy);
+    }
 }
