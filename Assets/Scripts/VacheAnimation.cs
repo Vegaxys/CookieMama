a10080cbc88f3ee6e4a4b64c7d3fba29f8493e13
Julien <julien.kopoin@gmail.com>
2019-06-06 13:47:46 +0200

    skybox

---
+++ Assets/Scripts/VacheAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VacheAnimation : MonoBehaviour{
+    private  Animator anim;
+    private void Start() {
+        anim = GetComponent<Animator>();
+        StartCoroutine(RandomAnimation(1));
+    }
+    private IEnumerator RandomAnimation(float random) {
+        yield return new WaitForSeconds(random);
+        float _random = Random.Range(2f, 5f);
+        anim.SetTrigger("Change");
+        StartCoroutine(RandomAnimation(_random));
+    }
+}
