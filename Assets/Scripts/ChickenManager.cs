0a426046243393d8a0a795978a854247f21d390d
Julien <julien.kopoin@gmail.com>
2019-06-07 14:16:08 +0200

    particules et Poulet AI

---
+++ Assets/Scripts/ChickenManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenManager : MonoBehaviour{
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireCube(transform.position, new Vector3(transform.localScale.x * 10, transform.localScale.y * 10, transform.localScale.z * 10));
+    }
+    public Vector3 FindNewPos() {
+        Vector3 newPos = new Vector3(
+            transform.position.x + Random.Range(-transform.localScale.x * 5, transform.localScale.x * 5),
+            0,
+            transform.position.z + Random.Range(-transform.localScale.z * 5, transform.localScale.z * 5));
+        return newPos;
+    }
+}
--- Assets/Scripts/ChickenScript.cs
+++ Assets/Scripts/ChickenScript.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class ChickenScript : MonoBehaviour
 {
     private Animator anim;
+    public ChickenManager manager;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         StartCoroutine(RandomAnimation(1));
+        StartCoroutine(MoveTo(manager.FindNewPos()));
     }
+    IEnumerator MoveTo(Vector3 position) {
+        while(Vector3.Distance(transform.position, position) > 2) {
+            transform.position = Vector3.MoveTowards(transform.position, position, Time.deltaTime * 3);
+            transform.LookAt(position);
+            transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+            yield return null;
+        }
+        StartCoroutine(MoveTo(manager.FindNewPos()));
+    }
+
+
 
     private IEnumerator RandomAnimation(float random)
     {
